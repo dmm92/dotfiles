22447501e363dde7491552e886b0f957f593c2fe
Daniel Miller-Medzon <dmm@Groot.local>
2018-09-14 16:40:01 -0400

    brew changes

--- brew.sh
+++ brew.sh
@@ -43,41 +43,8 @@ brew install openssh
 brew install screen
 brew install homebrew/php/php56 --with-gmp
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
@@ -93,9 +60,14 @@ brew install tree
 brew install vbindiff
 brew install zopfli
 
-# My stuff
-brew install python2
-
+# Personal additions
+brew install python@2
+brew install awscli
+brew install awslogs
+brew install jq
+brew install gradle
+brew install gradle-completion
+brew cask install google-featured-photos
 
 # Remove outdated versions from the cellar.
 brew cleanup
